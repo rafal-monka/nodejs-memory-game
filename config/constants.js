16b5cb7492dd1c33515920fcf9999c96f602719b
Rafal Monka <monka.rafal@gmail.com>
2020-08-05 20:57:21 +0200

    WAIT_SECONDS 3

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,10 @@
 
+require('dotenv').config()
+
 const constants = {
     CONST_OPEN: -111,
     CONST_TAKEN: -999,
-    CONST_WAITING_TIME: 1000,
+    CONST_WAITING_TIME: process.env.WAIT_SECONDS*1000,
     CONST_SIZE: 8,
     CONST_DIVIDER: 200
 }
